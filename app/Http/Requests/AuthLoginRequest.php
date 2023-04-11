a6775292848ddb5f2dddc9340a57c194bbc30f02
Juergen Batubara <juergenkliensman06@gmail.com>
2023-04-11 22:30:06 +0700

    create api login and add middleware

---
+++ app/Http/Requests/AuthLoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|max:50',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'errors' => $validator->errors()->all(),
+        ], 400);
+
+        throw new ValidationException($validator, $response);
+    }
+}
